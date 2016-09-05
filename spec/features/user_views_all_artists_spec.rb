a642ca24f7c87d7aa2f000499beb89967b514ce4
David Davydov <kctrlv@gmail.com>
2016-09-05 13:47:11 -0600

    feature user can view all artists implemented

--- spec/features/user_views_all_artists_spec.rb
+++ spec/features/user_views_all_artists_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.feature "User views all artists" do
   scenario "they see list of artist names and links to their page" do
-    artists = %w(Adam Bob Chris) do |artist_name|
+    artists = %w(Adam Bob Chris).map do |artist_name|
       Artist.create(name: artist_name, image_path: 'http://theillixer.com/wp-content/uploads/2014/11/beta14-phase1-nominees-16x9-BestNewArtist.png')
     end
-
     visit '/artists'
+    save_and_open_page
 
     artists.each do |artist|
       expect(page).to have_link artist.name, href: artist_path(artist)
