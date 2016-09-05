2f54b8c75c7c25d980e42c9d8985fc8630ce18d1
David Davydov <kctrlv@gmail.com>
2016-09-05 14:06:20 -0600

    feature test user deletes artist storylined

---
+++ spec/features/user_deletes_an_artist_spec.rb
@@ -0,0 +1,12 @@
+RSpec.feature "User deletes an artist" do
+  scenario "they return to artist index page and don't see deleted artist" do
+
+    # As a user
+    # Given that an artist exists in the database
+    # When I visit that artist's show page
+    # And I click on "Delete"
+    # Then I should be back on the artist index page
+    # Then I should not see the artist's name
+
+  end
+end
