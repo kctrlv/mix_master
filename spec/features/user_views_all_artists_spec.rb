d26edfa87372a24076bed69a6a7c82fc3257900d
David Davydov <kctrlv@gmail.com>
2016-09-05 13:28:43 -0600

    feature test user sees all artist written

---
+++ spec/features/user_views_all_artists_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "User views all artists" do
+  scenario "they see list of artist names and links to their page" do
+    artists = %w(Adam Bob Chris) do |artist_name|
+      Artist.create(name: artist_name, image_path: 'http://theillixer.com/wp-content/uploads/2014/11/beta14-phase1-nominees-16x9-BestNewArtist.png')
+    end
+
+    visit '/artists'
+
+    artists.each do |artist|
+      expect(page).to have_link artist.name, href: artist_path(artist)
+    end
+  end
+end
