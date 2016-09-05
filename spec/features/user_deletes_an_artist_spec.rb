d013c7a7f827b0397e59d464088fe47d4ea0a670
David Davydov <kctrlv@gmail.com>
2016-09-05 14:09:54 -0600

    feature test user deletes artist written

--- spec/features/user_deletes_an_artist_spec.rb
+++ spec/features/user_deletes_an_artist_spec.rb
@@ -3,10 +3,14 @@ RSpec.feature "User deletes an artist" do
 
     # As a user
     # Given that an artist exists in the database
+    artist = Artist.create(name: "Coolio", image_path: "http://i.imgur.com/WMaRmMA.jpg")
     # When I visit that artist's show page
+    visit artist_path(artist)
     # And I click on "Delete"
+    click_on "Delete"
     # Then I should be back on the artist index page
+    expect(page).to have_content "All Artists"
     # Then I should not see the artist's name
-
+    expect(page).not_to have_content "Coolio"
   end
 end
